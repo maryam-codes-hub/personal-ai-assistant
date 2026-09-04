ffd820e3aca35d800474866fe90317282157e451
maryam-codes-hub <maryamehsan308@gmail.com>
2026-09-04 15:43:11 +0500

    date,time feature added

--- main.py
+++ main.py
@@ -1,7 +1,18 @@
+from datetime import date
+import time
 name=input("Enter User name:")
+
 print(f"Hello {name}! i am your personal AI Assitant")
 command=input("Enter Command:")
-if command=="exit":
+if command=="time":
+    current=time.ctime()
+    print(f"Current time is :{current}")
+elif command=="date":
+    today=date.today()
+    print(f"Today's date:{today}")
+
+
+elif command=="exit":
     print("Goodbye")
 else:
     print("I dont understand this command")
