085366e5fb89b6d211d20cfe5f753ce908ef5138
maryam-codes-hub <maryamehsan308@gmail.com>
2026-09-04 14:58:50 +0500

    greetings added

---
+++ main.py
@@ -0,0 +1,7 @@
+name=input("Enter User name:")
+print(f"Hello {name}! i am your personal AI Assitant")
+command=input("Enter Command:")
+if command=="exit":
+    print("Goodbye")
+else:
+    print("I dont understand this command")
